7fd912fa251fb5509c7ca881beed579138d5f625
Jeff Snyder <jeff@snyderphonics.com>
2018-12-17 21:48:44 -0500

    first commit of leaf genera template - working dual sine wave fm from audio input example

--- H7-cubeSW/Src/adc.c
+++ H7-cubeSW/Src/adc.c
@@ -4,35 +4,45 @@
   * Description        : This file provides code for the configuration
   *                      of the ADC instances.
   ******************************************************************************
-  ** This notice applies to any and all portions of this file
+  * This notice applies to any and all portions of this file
   * that are not between comment pairs USER CODE BEGIN and
   * USER CODE END. Other portions of this file, whether 
   * inserted by the user or by software development tools
   * are owned by their respective copyright owners.
   *
-  * COPYRIGHT(c) 2018 STMicroelectronics
+  * Copyright (c) 2018 STMicroelectronics International N.V. 
+  * All rights reserved.
   *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
   *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
   ******************************************************************************
   */
@@ -74,7 +84,7 @@ void MX_ADC1_Init(void)
   hadc1.Init.Overrun = ADC_OVR_DATA_PRESERVED;
   hadc1.Init.LeftBitShift = ADC_LEFTBITSHIFT_NONE;
   hadc1.Init.BoostMode = ENABLE;
-  hadc1.Init.OversamplingMode = DISABLE;
+  hadc1.Init.OversamplingMode = DISABLE;  //Might be worth checking out the oversampling feature - there is an H7 firmware example online - JS
   if (HAL_ADC_Init(&hadc1) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -88,117 +98,117 @@ void MX_ADC1_Init(void)
     _Error_Handler(__FILE__, __LINE__);
   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_5;
-  sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_32CYCLES_5;
-  sConfig.SingleDiff = ADC_SINGLE_ENDED;
-  sConfig.OffsetNumber = ADC_OFFSET_NONE;
-  sConfig.Offset = 0;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+  /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_5;
+   sConfig.Rank = ADC_REGULAR_RANK_1;
+   sConfig.SamplingTime = ADC_SAMPLETIME_32CYCLES_5;
+   sConfig.SingleDiff = ADC_SINGLE_ENDED;
+   sConfig.OffsetNumber = ADC_OFFSET_NONE;
+   sConfig.Offset = 0;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_9;
-  sConfig.Rank = ADC_REGULAR_RANK_2;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_9;
+   sConfig.Rank = ADC_REGULAR_RANK_2;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_8;
-  sConfig.Rank = ADC_REGULAR_RANK_3;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_8;
+   sConfig.Rank = ADC_REGULAR_RANK_3;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_4;
-  sConfig.Rank = ADC_REGULAR_RANK_4;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_4;
+   sConfig.Rank = ADC_REGULAR_RANK_4;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_7;
-  sConfig.Rank = ADC_REGULAR_RANK_5;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_7;
+   sConfig.Rank = ADC_REGULAR_RANK_5;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_3;
-  sConfig.Rank = ADC_REGULAR_RANK_6;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_3;
+   sConfig.Rank = ADC_REGULAR_RANK_6;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_15;
-  sConfig.Rank = ADC_REGULAR_RANK_7;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_15;
+   sConfig.Rank = ADC_REGULAR_RANK_7;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_14;
-  sConfig.Rank = ADC_REGULAR_RANK_8;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_14;
+   sConfig.Rank = ADC_REGULAR_RANK_8;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_17;
-  sConfig.Rank = ADC_REGULAR_RANK_9;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_17;
+   sConfig.Rank = ADC_REGULAR_RANK_9;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_16;
-  sConfig.Rank = ADC_REGULAR_RANK_10;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_16;
+   sConfig.Rank = ADC_REGULAR_RANK_10;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_19;
-  sConfig.Rank = ADC_REGULAR_RANK_11;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_19;
+   sConfig.Rank = ADC_REGULAR_RANK_11;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_18;
-  sConfig.Rank = ADC_REGULAR_RANK_12;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
+     /**Configure Regular Channel
+     */
+   sConfig.Channel = ADC_CHANNEL_18;
+   sConfig.Rank = ADC_REGULAR_RANK_12;
+   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+   {
+     _Error_Handler(__FILE__, __LINE__);
+   }
 
 }
 
@@ -224,9 +234,9 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     PA6     ------> ADC1_INP3
     PA7     ------> ADC1_INP7
     PC4     ------> ADC1_INP4
-    PC5     ------> ADC1_INP8
+    PC5     ------> ADC1_INP8 
     PB0     ------> ADC1_INP9
-    PB1     ------> ADC1_INP5 
+    PB1     ------> ADC1_INP5
     */
     GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
@@ -283,6 +293,7 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
     /* Peripheral clock disable */
     __HAL_RCC_ADC12_CLK_DISABLE();
   
+
     /**ADC1 GPIO Configuration    
     PA0-WKUP     ------> ADC1_INP16
     PA1     ------> ADC1_INP17
@@ -293,10 +304,11 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
     PA6     ------> ADC1_INP3
     PA7     ------> ADC1_INP7
     PC4     ------> ADC1_INP4
-    PC5     ------> ADC1_INP8
+    PC5     ------> ADC1_INP8 
     PB0     ------> ADC1_INP9
-    PB1     ------> ADC1_INP5 
+    PB1     ------> ADC1_INP5
     */
+
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3 
                           |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
 
--- H7-cubeSW/Src/audiostream.c
+++ H7-cubeSW/Src/audiostream.c
@@ -1,86 +1,87 @@
 /* Includes ------------------------------------------------------------------*/
 #include "audiostream.h"
 #include "main.h"
+#include "leaf.h"
 #include "codec.h"
+#include "tim.h"
 
-#define AUDIO_FRAME_SIZE     32
-#define HALF_BUFFER_SIZE      AUDIO_FRAME_SIZE * 2 //number of samples per half of the "double-buffer" (twice the audio frame size because there are interleaved samples for both left and right channels)
-#define AUDIO_BUFFER_SIZE     AUDIO_FRAME_SIZE * 4 //number of samples in the whole data structure (four times the audio frame size because of stereo and also double-buffering/ping-ponging)
-
-// align is to make sure they are lined up with the data boundaries of the cache 
-// at(0x3....) is to put them in the D2 domain of SRAM where the DMA can access them
-// (otherwise the TX times out because the DMA can't see the data location) -JS
-
-
-ALIGN_32BYTES (int16_t audioOutBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2);
-ALIGN_32BYTES (int16_t audioInBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2);
-
-
-int16_t inBuffer[HALF_BUFFER_SIZE];
-int16_t outBuffer[HALF_BUFFER_SIZE];
-
-uint16_t* adcVals;
-
-uint8_t buttonAPressed = 0;
-
-float sample = 0.0f;
+//the audio buffers are put in the D2 RAM area because that is a memory location that the DMA has access to.
+int32_t audioOutBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2;
+int32_t audioInBuffer[AUDIO_BUFFER_SIZE] __ATTR_RAM_D2;
 
 void audioFrame(uint16_t buffer_offset);
 float audioTickL(float audioIn); 
 float audioTickR(float audioIn);
 void buttonCheck(void);
 
+HAL_StatusTypeDef transmit_status;
+HAL_StatusTypeDef receive_status;
+
+uint16_t* adcVals;
+
+
 int mode1 = 1;
 int mode2 = 1;
 int mode3 = 1;
+int RGB_mode = 3;
 
-tRamp* adc[8];
-tCycle* mySine[2];
+#define NUM_BUTTONS 4
+uint8_t buttonValues[NUM_BUTTONS];
+uint8_t buttonValuesPrev[NUM_BUTTONS];
+uint32_t buttonCounters[NUM_BUTTONS];
+uint32_t buttonPressed[NUM_BUTTONS];
 
-HAL_StatusTypeDef transmit_status;
-HAL_StatusTypeDef receive_status;
+
+float sample = 0.0f;
+
+uint16_t frameCounter = 0;
+
+//audio objects
+tRamp adc[8];
+tCycle mySine[2];
+
+/**********************************************/
 
 typedef enum BOOL {
 	FALSE = 0,
 	TRUE
 } BOOL;
 
-void audioInit(I2C_HandleTypeDef* hi2c, SAI_HandleTypeDef* hsaiOut, SAI_HandleTypeDef* hsaiIn, RNG_HandleTypeDef* hrand, uint16_t* myADCArray)
-{ 
-	// Initialize the audio library. OOPS.
-	OOPSInit(SAMPLE_RATE, &randomNumber);
-	
+
+void audioInit(I2C_HandleTypeDef* hi2c, SAI_HandleTypeDef* hsaiOut, SAI_HandleTypeDef* hsaiIn, RNG_HandleTypeDef* hrand, uint16_t* adc_array)
+{
+	// Initialize LEAF.
+
+	LEAF_init(SAMPLE_RATE, AUDIO_FRAME_SIZE, &randomNumber);
+
+	adcVals = adc_array; //in audiostream.h, the array of adc values is now called adcVals.
+
+	for (int i = 0; i < 8; i++)
+	{
+		tRamp_init(&adc[i],7.0f, 1); //set all ramps for knobs to be 7ms ramp time and let the init function know they will be ticked every sample
+	}
+
+	tCycle_init(&mySine[0]);
+	tCycle_init(&mySine[1]);
 	//now to send all the necessary messages to the codec
 	AudioCodec_init(hi2c);
 
 	HAL_Delay(100);
 
-	adcVals = myADCArray;
-
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < AUDIO_BUFFER_SIZE; i++)
 	{
-		adc[i] = tRampInit(12.0f, 1);
+		audioOutBuffer[i] = 0;
 	}
 
-	mySine[0] = tCycleInit();
-	mySine[1] = tCycleInit();
-
-	HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-	// set up the I2S driver to send audio data to the codec (and retrieve input as well)	
+	// set up the I2S driver to send audio data to the codec (and retrieve input as well)
 	transmit_status = HAL_SAI_Transmit_DMA(hsaiOut, (uint8_t *)&audioOutBuffer[0], AUDIO_BUFFER_SIZE);
 	receive_status = HAL_SAI_Receive_DMA(hsaiIn, (uint8_t *)&audioInBuffer[0], AUDIO_BUFFER_SIZE);
-
 }
-
-uint16_t frameCounter = 0;
 
 void audioFrame(uint16_t buffer_offset)
 {
-	uint16_t i = 0;
-	int16_t current_sample = 0;
-
+	int i;
+	int32_t current_sample = 0;
 
 	frameCounter++;
 	if (frameCounter >= 1)
@@ -88,40 +89,42 @@ void audioFrame(uint16_t buffer_offset)
 		frameCounter = 0;
 		buttonCheck();
 	}
-	
+
 	for (i = 0; i < (HALF_BUFFER_SIZE); i++)
 	{
-		if ((i & 1) == 0) {
-			current_sample = (int16_t)(audioTickL((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_15)) * TWO_TO_15);
+		if ((i & 1) == 0)
+		{
+			current_sample = (int32_t)(audioTickL((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_31)) * TWO_TO_31);
 		}
 		else
 		{
-			current_sample = (int16_t)(audioTickR((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_15)) * TWO_TO_15);
+			current_sample = (int32_t)(audioTickR((float) (audioInBuffer[buffer_offset + i] * INV_TWO_TO_31)) * TWO_TO_31);
 		}
+
 		audioOutBuffer[buffer_offset + i] = current_sample;
 	}
 }
 
-
-float audioTickL(float audioIn) 
+float audioTickL(float audioIn)
 {
-	tRampSetDest(adc[0], 1.0f - (adcVals[0] * INV_TWO_TO_16));
-	tRampSetDest(adc[4], 1.0f - (adcVals[4] * INV_TWO_TO_16));
-	float newFreq = OOPS_midiToFrequency(tRampTick(adc[0]) * 127.0f) + (audioIn * tRampTick(adc[4]) * 1000.0f);
-	tCycleSetFreq(mySine[0], newFreq);
-	sample = tCycleTick(mySine[0]);
+	tRamp_setDest(&adc[0], 1.0f - (adcVals[0] * INV_TWO_TO_16));
+	tRamp_setDest(&adc[4], 1.0f - (adcVals[4] * INV_TWO_TO_16));
+	float newFreq = LEAF_midiToFrequency(tRamp_tick(&adc[0]) * 127.0f) + (audioIn * tRamp_tick(&adc[4]) * 1000.0f);
+	tCycle_setFreq(&mySine[0], newFreq);
+	sample = tCycle_tick(&mySine[0]);
 	return sample * .9f;
 }
 
-float audioTickR(float audioIn) 
+float audioTickR(float audioIn)
 {
-	tRampSetDest(adc[1], 1.0f -  (adcVals[1] * INV_TWO_TO_16));
-	tRampSetDest(adc[5], 1.0f - (adcVals[5] * INV_TWO_TO_16));
-	float newFreq = OOPS_midiToFrequency(tRampTick(adc[1]) * 127.0f) + (audioIn * tRampTick(adc[5]) * 1000.0f);
-	tCycleSetFreq(mySine[1], newFreq);
-	sample = tCycleTick(mySine[1]);
+	tRamp_setDest(&adc[1], 1.0f - (adcVals[1] * INV_TWO_TO_16));
+	tRamp_setDest(&adc[5], 1.0f - (adcVals[5] * INV_TWO_TO_16));
+	float newFreq = LEAF_midiToFrequency(tRamp_tick(&adc[1]) * 127.0f) + (audioIn * tRamp_tick(&adc[5]) * 1000.0f);
+	tCycle_setFreq(&mySine[1], newFreq);
+	sample = tCycle_tick(&mySine[1]);
 	return sample * .9f;
 }
+
 
 void buttonCheck(void)
 {
@@ -149,32 +152,44 @@ void buttonCheck(void)
 
 	if (buttonPressed[0] == 1)
 	{
+		RGB_mode++;
+		if (RGB_mode > 3)
+		{
+			RGB_mode = 0;
+		}
+		if (RGB_mode == 0)
+		{
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 9000); //R   ///the red channel values should be between 8000 and 16000
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0); //G
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0); //B
+		}
 
+		else if (RGB_mode == 1)
+		{
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 00); //R
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 300); //G   //green channel values between 100 and 500
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0); //B
+		}
 
-			/*
+		else if (RGB_mode == 2)
+		{
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 00); //R
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 00); //G
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 200); //B   // blue channel between 0 and 400
+		}
 
-			//RGB LED controls
-			//R
-				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-
-			//G
-				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_RESET);
-
-			//B
-				HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_RESET);
-				HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9, GPIO_PIN_SET);
-
-
-			*/
-			buttonPressed[0] = 0;
+		else if (RGB_mode == 3)
+		{
+			__HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, 0); //R
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 0); //G
+			__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, 0); //B
+		}
+		buttonPressed[0] = 0;
 	}
 	if (buttonPressed[1] == 1)
 	{
+		//currently the dimming function of the PWM timers overrides the use of the LED pins as digital outputs
+		/*
 		if (mode1 == 0)
 		{
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);
@@ -185,11 +200,13 @@ void buttonCheck(void)
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_SET);
 			mode1 = 0;
 		}
+		*/
 		buttonPressed[1] = 0;
 	}
 	if (buttonPressed[2] == 1)
 	{
-
+		//currently the dimming function of the PWM timers overrides the use of the LED pins as digital outputs
+		/*
 		if (mode2 == 0)
 		{
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_RESET);
@@ -200,11 +217,13 @@ void buttonCheck(void)
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11, GPIO_PIN_SET);
 			mode2 = 0;
 		}
+		*/
 		buttonPressed[2] = 0;
 	}
 
 	if (buttonPressed[3] == 1)
 	{
+		//Timer PWM isn't set up on this LED pin, so we can just set it to on/off.
 		if (mode3 == 0)
 		{
 			HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
@@ -219,10 +238,9 @@ void buttonCheck(void)
 	}
 }
 
-
 void HAL_SAI_ErrorCallback(SAI_HandleTypeDef *hsai)
 {
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);
+	;
 }
 
 void HAL_SAI_TxCpltCallback(SAI_HandleTypeDef *hsai)
@@ -235,17 +253,13 @@ void HAL_SAI_TxHalfCpltCallback(SAI_HandleTypeDef *hsai)
   ;
 }
 
+
 void HAL_SAI_RxCpltCallback(SAI_HandleTypeDef *hsai)
 {
-
 	audioFrame(HALF_BUFFER_SIZE);
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, GPIO_PIN_SET);;
 }
 
 void HAL_SAI_RxHalfCpltCallback(SAI_HandleTypeDef *hsai)
 {
-
 	audioFrame(0);
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10, GPIO_PIN_RESET);;
 }
-
