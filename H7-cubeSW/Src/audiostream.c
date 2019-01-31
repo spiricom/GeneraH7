f971ee6b69475d51d1233decd0e86393e42baa60
Jeff Snyder <jeff@snyderphonics.com>
2019-01-31 15:24:20 -0500

    fixed order of ADC inputs so that they match the comment that explains them

--- H7-cubeSW/Src/audiostream.c
+++ H7-cubeSW/Src/audiostream.c
@@ -56,6 +56,24 @@ void audioInit(I2C_HandleTypeDef* hi2c, SAI_HandleTypeDef* hsaiOut, SAI_HandleTy
 
 	adcVals = adc_array; //in audiostream.h, the array of adc values is now called adcVals.
 
+	// adcVals array has the data organized as follows:
+		// [0] = knob 1
+		// [1] = knob 2
+		// [2] = knob 3
+		// [3] = knob 4
+		// [4] = knob 5 (depending on jumper K)
+		// [5] = knob 6 (depending on jumper L)
+		// [6] = knob 7 or jack 11 (depending on jumper M)
+		// [7] = knob 8 or jack 12 (depending on jumper N and jumper O)
+		// [8] = jack 1
+		// [9] = jack 2
+		// [10] = jack 3 (depending on jumper A and jumper B)
+		// [11] = jack 4 (depending on jumper C and jumper D)
+
+		// note that the knobs come in with the data backwards (fully clockwise is near zero, counter-clockwise is near 65535)
+		// the CVs come in as expected (0V = 0, 10V = 65535)
+
+
 	for (int i = 0; i < 8; i++)
 	{
 		tRamp_init(&adc[i],7.0f, 1); //set all ramps for knobs to be 7ms ramp time and let the init function know they will be ticked every sample
@@ -118,13 +136,11 @@ float audioTickL(float audioIn)
 float audioTickR(float audioIn)
 {
 	sample = audioIn;
-	/*
 	tRamp_setDest(&adc[1], 1.0f - (adcVals[1] * INV_TWO_TO_16));
 	tRamp_setDest(&adc[5], 1.0f - (adcVals[5] * INV_TWO_TO_16));
 	float newFreq = LEAF_midiToFrequency(tRamp_tick(&adc[1]) * 127.0f) + (audioIn * tRamp_tick(&adc[5]) * 1000.0f);
 	tCycle_setFreq(&mySine[1], newFreq);
 	sample = tCycle_tick(&mySine[1]);
-	*/
 	return sample * .9f;
 }
 
